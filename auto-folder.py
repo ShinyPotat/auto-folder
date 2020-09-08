0bb598ca01d98c048a4b08da0bf7c73038ed23b5
ShinyPotat <59798595+ShinyPotat@users.noreply.github.com>
2020-09-08 20:17:36 +0200

    prints mejorados con colorama

--- auto-folder.py
+++ auto-folder.py
@@ -1,20 +1,13 @@
 import os, sys, getopt, json
+from colorama import init, Fore
 
-class bcolors:
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
+init(autoreset=True)
 
 try:
     with open('./extensions.json') as f:
         extensions = json.load(f)
 except FileNotFoundError:
-    print(bcolors.FAIL + 'Error: extensions.json file must be in the same folder than auto-folder.py' + bcolors.ENDC)
+    print(Fore.RED + 'Error: extensions.json file must be in the same folder than auto-folder.py' + Fore.RESET)
     sys.exit(2)
 
 folders = list(extensions.keys())
@@ -24,13 +17,13 @@ def main(argv):
 
     #Comprobar los valores introducidos por los usuarios
     if len(argv) == 0 or len(argv) > 2 or argv[0].startswith('-')==False:
-        print(bcolors.FAIL + '<Syntax error>: ' + bcolors.ENDC + 'auto-folder.py -p <path>')
+        print(Fore.RED + '<Syntax error>: ' + Fore.RESET + 'auto-folder.py -p <path>')
         sys.exit(2) 
 
     try:
         opts, _args = getopt.getopt(argv,"hp:")
     except getopt.GetoptError:
-        print(bcolors.FAIL + '<Syntax error>: ' + bcolors.ENDC + 'auto-folder.py -p <path>')
+        print(Fore.RED + '<Syntax error>: ' + Fore.RESET + 'auto-folder.py -p <path>')
         sys.exit(2)
 
     #Comprobar el flag que se ha usado
@@ -45,7 +38,7 @@ def main(argv):
     for folder in folders:
         if(os.path.isdir(os.path.join(path, folder))==False):
             os.mkdir(os.path.join(path, folder))
-            print('folder ' + bcolors.OKGREEN + folder + bcolors.ENDC + ' created')
+            print('folder ' + Fore.GREEN + folder + Fore.RESET + ' created')
 
     while True:
 
@@ -63,13 +56,13 @@ def main(argv):
 
                 if file_extension in extensions.get(type):
                     os.rename(os.path.join(path, file), os.path.join(new_path, file))
-                    print(bcolors.FAIL + file + bcolors.ENDC + ' moved to ' + bcolors.OKGREEN + type + bcolors.ENDC)
+                    print(Fore.RED + file + Fore.RESET + ' moved to ' + Fore.GREEN + type + Fore.RESET)
                     break
             
             if(os.path.isfile(os.path.join(path, file))):
                 new_path = os.path.join(path, 'other')
                 os.rename(os.path.join(path, file), os.path.join(new_path, file))
-                print(bcolors.FAIL + file + bcolors.ENDC + ' moved to ' + bcolors.OKGREEN + 'other' + bcolors.ENDC)            
+                print(Fore.RED + file + Fore.RESET + ' moved to ' + Fore.GREEN + 'other' + Fore.RESET)            
 
 if __name__ == "__main__":
     main(sys.argv[1:])
